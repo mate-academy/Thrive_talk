283a2d66fb5fd5a8435cd7526400f9eb9f12ebc7
Nikolay Filiuk <Filiuk98@gmail.com>
2020-09-17 16:44:25 +0300

    v1

---
+++ src/scripts/menu.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const toggleMenu = (() => {
+  const navToggler = document.querySelector('.nav-toggler');
+  const nav = document.querySelector('.header__nav');
+  const links = document.querySelectorAll('.header__link');
+  let activeLink = 0;
+
+  for (let i = 0; i < links.length; i++) {
+    links[i].onclick = () => {
+      navToggler.classList.toggle('nav-toggler--active');
+      nav.classList.toggle('header__nav--active');
+      links[activeLink].classList.toggle('header__link--active');
+      links[i].classList.toggle('header__link--active');
+      activeLink = i;
+    };
+  }
+
+  navToggler.onclick = () => {
+    navToggler.classList.toggle('nav-toggler--active');
+    nav.classList.toggle('header__nav--active');
+  };
+})();
