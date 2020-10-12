853aecf25d67c759cd211d54f0f846063d97c46a
Nikolay Filiuk <Filiuk98@gmail.com>
2020-10-12 16:24:28 +0300

    Adaptive for large screen

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -2,14 +2,14 @@
 
 (function toggleMenu() {
   const navToggler = document.querySelector('.nav-toggler');
-  const nav = document.querySelector('.header__nav');
+  const nav = document.querySelector('.header__navigation');
   const links = document.querySelectorAll('.header__link');
   let activeLink = 0;
 
   for (let i = 0; i < links.length; i++) {
     links[i].onclick = () => {
       navToggler.classList.toggle('nav-toggler--active');
-      nav.classList.toggle('header__nav--active');
+      nav.classList.toggle('header__navigation--active');
       links[activeLink].classList.toggle('header__link--active');
       links[i].classList.toggle('header__link--active');
       activeLink = i;
@@ -18,6 +18,6 @@
 
   navToggler.onclick = () => {
     navToggler.classList.toggle('nav-toggler--active');
-    nav.classList.toggle('header__nav--active');
+    nav.classList.toggle('header__navigation--active');
   };
 })();
