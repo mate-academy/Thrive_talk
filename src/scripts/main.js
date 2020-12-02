79e9a01505fb60952f6fb8e20f85d2a91d92c691
Maxim Zhuravlyov <maksym.zhuravlov1995@gmail.com>
2020-12-02 20:37:38 +0200

    Header and showcase partially implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+$(document).ready(function() {
+  // prevent .nav__item from widening on link hover
+  const array = $('.nav__link');
+
+  for (let i = 0; i < array.length; i++) {
+    array.eq(i).parent().css('width', array.eq(i).width() + 12);
+  }
+});
