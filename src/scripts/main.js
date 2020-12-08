20a6d2af45a03457a191fa61db901536805adb1d
Maryna Zinchenko <pandoz16@gmail.com>
2020-12-08 12:12:50 +0200

    burger-menu was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+(function() {
+  const burger = document.querySelector('.nav__burger');
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('nav__burger-active');
+  });
+}());
